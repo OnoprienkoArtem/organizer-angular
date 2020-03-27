186b20a32e62986ae6a07a50ea26bf991c2dd8d9
OnoprienkoArtem <o.artem.e@gmail.com>
2020-03-27 22:51:14 +0200

    create moment service

--- src/app/celector/celector.component.ts
+++ src/app/celector/celector.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DateService } from '../shared/date.service';
 
 @Component({
   selector: 'app-celector',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CelectorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dateService: DateService) {
+  }
 
   ngOnInit(): void {
   }
