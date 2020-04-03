9263a24f7a49114a801ef16705add23240ce0164
OnoprienkoArtem <o.artem.e@gmail.com>
2020-04-03 22:40:42 +0300

    add load method

--- src/app/shared/tasks.service.ts
+++ src/app/shared/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import * as moment from 'moment';
 
 export interface Task {
   id?: string;
@@ -21,6 +22,19 @@ export class TasksService {
   static url = '';
 
   constructor(private http: HttpClient) { }
+
+  load(date: moment.Moment): Observable<Task[]> {
+    return this.http
+      .get<Task[]>(`${TasksService.url}/${date.format('DD-MM-YYYY')}.json`)
+      .pipe(
+        map(tasks => {
+          if (!tasks) {
+            return [];
+          }
+          return Object.keys(tasks).map(key => ({...tasks[key], id: key}));
+        })
+      );
+  }
 
   create(task: Task): Observable<Task> {
     return this.http
